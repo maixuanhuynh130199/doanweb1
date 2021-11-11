4fbb27da39848097a297c99445c03353249f4753
Mai Xuan Huynh <19211tt2049@mail.tdc.edu.vn>
2021-11-11 12:56:58 +0700

    update file

--- src/app/product-edit/product-edit.component.spec.ts
+++ src/app/product-edit/product-edit.component.spec.ts
@@ -6,20 +6,5 @@ describe('ProductEditComponent', () => {
   let component: ProductEditComponent;
   let fixture: ComponentFixture<ProductEditComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProductEditComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProductEditComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+ 
 });
